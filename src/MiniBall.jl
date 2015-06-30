7278f45fa04af86e0dbdeffd8b6c49a272515420
ovainola <olli.vainola@student.oulu.fi>
2015-06-30 18:02:54 +0300

    fixed bug

--- src/MiniBall.jl
+++ src/MiniBall.jl
@@ -9,6 +9,12 @@ cxxinclude(path_to_miniball)
 cxx"""
 #include <iostream>
 
+/*
+ * Function for allocating array for miniball
+ *
+ * Since arrays in Julia are comprehended as 1D arrays when trasported into
+ * C++ (I guess) we'll allocate n dimensional array for miniball
+ */ 
 template <typename N>
 N** allocate_c_arr(int length, int width, N * juliaArr) {
     N** output_arr = new N*[length];
@@ -24,6 +30,11 @@ N** allocate_c_arr(int length, int width, N * juliaArr) {
     return output_arr;
 }
 
+/*
+ * Free allocated array
+ *
+ * Just in case...  
+ */
 template <typename N>
 void free_c_array(int length, N** c_arr) {
     for (int i = 0; i < length; i++)
@@ -31,6 +42,9 @@ void free_c_array(int length, N** c_arr) {
     free(c_arr);
 }
 
+/*
+ * Calculates smallest enclosing sphere
+ */
 template <typename T>
 double calc_mini(int n, int d, T**arr, T *outputArr) {
     double radius;
@@ -39,22 +53,23 @@ double calc_mini(int n, int d, T**arr, T *outputArr) {
     typedef Miniball::Miniball <Miniball::CoordAccessor<PointIterator, CoordIterator> > MB;
     MB mb (n, arr, arr+d);
     const T* center = mb.center(); 
-    for(int i=0; i<d; ++i, ++center)
+    for(int i=0; i<n; ++i, ++center) {
         outputArr[i] = *center;
+    }
     radius = mb.squared_radius();
     return radius;
 }
 """
 
-allocate_jArr_to_cArr(length, width, juliaArr) = @cxx allocate_c_arr(length, width, juliaArr)
+allocate_jArr_to_cArr(length, width, juliaArr) = @cxxnew allocate_c_arr(length, width, juliaArr)
 calc_miniball(length, width, arr, outputArr) = @cxx calc_mini(length, width, arr, outputArr)
 free_cArr(length, c_arr) = @cxx free_c_array(length, c_arr)
 
-function miniball{T}(arr::Array{T, 2})
+function miniball{T<:Real}(arr::Array{T, 2})
     n, d = size(arr)
     output_arr = zeros(d)
     c_arr = allocate_jArr_to_cArr(n, d, pointer(arr'))
-    squared_radius = calc_miniball(n, d, c_arr, pointer(output_arr))
+    squared_radius = calc_miniball(d, n, c_arr, pointer(output_arr))
     free_cArr(n, c_arr)
     radius = sqrt(squared_radius)
     return output_arr, radius
