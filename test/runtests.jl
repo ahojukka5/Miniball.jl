3787288ba3fad2a3a3041e35ffd212bde1e223dd
Tero Frondelius <tero.frondelius@gmail.com>
2015-07-01 17:04:24 +0300

    tests added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using MiniBall
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+unit_circle_2D = miniball([-1.0 0.0; 1.0 0.0; 0.0 1.0; 0.0 -1.0])
+@test unit_circle_2D[2] == 1
+@test unit_circle_2D[1] == [0.0,0.0]
+
+unit_ball_3D = miniball([-1.0 0.0 0.0; 1.0 0.0 0.0; 0.0 1.0 0.0; 0.0 -1.0 0.0])
+@test unit_ball_3D[2] == 1
+@test unit_ball_3D[1] == [0.0,0.0,0.0]
