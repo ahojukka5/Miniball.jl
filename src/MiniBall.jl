b3f03b918cf380b96c1f5e81a95062ec6829f693
ovainola <olli.vainola@student.oulu.fi>
2015-07-02 14:17:09 +0300

    Miniball algorithm works only for floating points, added parent type for template

--- src/MiniBall.jl
+++ src/MiniBall.jl
@@ -72,12 +72,14 @@ free_cArr(length, c_arr) = @cxx free_c_array(length, c_arr)
 """
 Smallest enclosing sphere
 """
-function miniball{T}(arr::Array{T, 2})
+function miniball{T<:FloatingPoint}(arr::Array{T, 2})
     n, d = size(arr)
     output_arr = zeros(d)
+    radius = 0.0
     c_arr = allocate_jArr_to_cArr(n, d, pointer(arr'))
     squared_radius = calc_miniball(d, n, c_arr, pointer(output_arr))
     free_cArr(n, c_arr)
+    println(squared_radius)
     radius = sqrt(squared_radius)
     return output_arr, radius
 end
