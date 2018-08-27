5fbb58373addcf2abb001923abb0098a4ad96674
Jan Weidner <jw3126@gmail.com>
2018-08-27 20:47:24 +0200

    fix

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,28 +3,5 @@
 
 using Miniball
 using Base.Test
-using TimerOutputs
 
-pkg_dir = Pkg.dir("Miniball")
-maybe_test_files = readdir(joinpath(pkg_dir, "test"))
-is_test_file(fn) = startswith(fn, "test_") & endswith(fn, ".jl")
-test_files = filter(is_test_file, maybe_test_files)
-info("$(length(test_files)) test files found.")
-
-const to = TimerOutput()
-@testset "Miniball.jl" begin
-    for fn in test_files
-        info("----- Running tests from file $fn -----")
-        t0 = time()
-        timeit(to, fn) do
-            include(fn)
-        end
-        dt = round(time() - t0, 2)
-        info("----- Testing file $fn completed in $dt seconds -----")
-    end
-end
-
-println()
-println("Test statistics:")
-println(to)
-
+include("test_2d_points.jl")
