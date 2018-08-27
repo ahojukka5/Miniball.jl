f4dffedc9d19e6d1af90253f6ed368377442cc53
Jan Weidner <jw3126@gmail.com>
2018-08-27 20:27:04 +0200

    Final deprecation

--- src/Miniball.jl
+++ src/Miniball.jl
@@ -2,6 +2,7 @@
 # License is GPL: see https://github.com/JuliaFEM/MiniBall.jl/blob/master/LICENSE.md
 
 module Miniball
+    @warn("Miniball.jl is no longer maintained. Use BoundingSphere.jl instead")
 
     include("src.jl")
     export miniball
